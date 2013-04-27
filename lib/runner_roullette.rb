4d6fb84451788c769914367b01062cdf156502a4
rudedoc <finlay.mark@gmail.com>
2013-04-28 00:40:31 +0100

    refactoring

--- lib/runner_roullette.rb
+++ lib/runner_roullette.rb
@@ -2,13 +2,12 @@ require "runner_roullette/version"
 
 module RunnerRoullette
   class Runner
-    attr_accessor :name, :price, :price_decimal, :over_round
+    attr_accessor :name, :price, :price_decimal
 
     def initialize(name, price, price_decimal)
       @name = name
       @price = price
       @price_decimal = price_decimal
-      @over_round = over_round
     end
 
     def over_round
@@ -23,100 +22,73 @@ module RunnerRoullette
 
   class Table
     attr_accessor :runners
-    attr_reader :horizontal_row_prices
-    attr_reader :vertical_row_prices
-    attr_reader :red_runners_price
-    attr_reader :black_runners_price
+    attr_reader :horizontal_row_prices, :vertical_row_prices
 
     def initialize(runners)
       @runners = runners
       @horizontal_row_prices = horizontal_row_prices
       @vertical_row_prices = vertical_row_prices
-      @red_runners_price = red_runners_price
-      @black_runners_price = black_runners_price
     end
 
-
-    ### CALCULATE RED RUNNERS PRICE
-
-    # Get the array of red runners
     def red_runners
-      # Start with empty array container
-      red_runners_array = []
-      # Loop through all the runners in the runner array and their index position in runners array
-      @runners.each_with_index do |runner, index|
-        # if the runner's index position is an even number add it to the red_runner_array
-        if index.even? then red_runners_array << runner end
+      array = []
+      runners.each_with_index do |runner, index|
+        if index.even? then array << runner end
       end
-      # Return the array of red runners
-      red_runners_array
+      array
     end
 
-    # Calculate the red runners price
-    def red_runners_price
-      # Start with a total over round of 0
-      total_over_round = 0
-      # Loop through the array of red runners
-      red_runners.each do |runner|
-        # Add each runners over_round to the total
-        total_over_round += runner.over_round
-      end
-      # The price is the total_over_round divided by 100
-      price = (100 / total_over_round)
-      # Round out the decimal to 2 places
-      price.round(2)
-    end
-
-    ## CALCULATE BLACK RUNNERS PRICES
-    # Same as Red Runners - will refactor to remove duplication
     def black_runners
       array = []
-      @runners.each_with_index do |runner, index|
+      runners.each_with_index do |runner, index|
         if index.odd? then array << runner end
       end
       array
     end
 
+    def red_runners_price
+      line_price(red_runners)
+    end
+
     def black_runners_price
+      line_price(black_runners)
+    end
+
+    def line_price(runners)
       total_over_round = 0
-      black_runners.each do |runner|
+      runners.each do |runner|
         total_over_round += runner.over_round
       end
       price = (100 / total_over_round)
       price.round(2)
     end
 
-    # Array of Vertical Row Prices
     def vertical_row_prices
-      row_prices(vertical_rows)
+      row_prices(columns)
+      # each column - gen a price and store it in array
     end
 
-    # Array of Horizontal Row Prices
     def horizontal_row_prices
-      row_prices(horizontal_rows)
+      row_prices(rows)
     end
 
-    # All possible Horizontal Rows
-    def horizontal_rows
-      @runners.each_slice(3).to_a
+    def rows
+      runners.each_slice(3).to_a
     end
 
-    # All Possible Vertical Rows
-    def vertical_rows
-      vertical_rows = []
-      horizontal_rows[0].count.times do |x|
-        vertical_row = []
-        horizontal_rows.count.times do |y|
-          if horizontal_rows[y][x] != nil
-            vertical_row << horizontal_rows[y][x]
+    def columns
+      columns = []
+      rows[0].count.times do |x|
+        column = []
+        rows.count.times do |y|
+          if rows[y][x] != nil
+            column << rows[y][x]
           end
         end
-        vertical_rows << vertical_row
+        columns << column
       end
-      vertical_rows
+      columns
     end
-
-    private
 
     def row_prices(rows)
       rows_prices = []
--- spec/table_spec.rb
+++ spec/table_spec.rb
@@ -36,25 +36,25 @@ describe RunnerRoullette::Table do
   end
 
   it "should have 8 rows" do
-    table.horizontal_rows.count.should == 8
+    table.rows.count.should == 8
   end
 
   it "should have 3 columns in each full row" do
-    table.horizontal_rows[0..6].each do |row|
+    table.rows[0..6].each do |row|
       row.count.should == 3
     end
   end
 
   its "first square/runner on the table should have a price of 1/1" do
-    table.horizontal_rows[0][0].price.should == "1/1"
+    table.rows[0][0].price.should == "1/1"
   end
 
   its "first square/runner on the table should have an over round % of 50%" do
-    table.horizontal_rows[0][0].over_round.should == 50.00
+    table.rows[0][0].over_round.should == 50.00
   end
 
   its "3rd square/runner on the table should have an over round of 25%" do
-    table.horizontal_rows[0][2].over_round.should == 25.00
+    table.rows[0][2].over_round.should == 25.00
   end
 
   its "1st row bet should have a price_decimal of 1.92" do
@@ -66,15 +66,15 @@ describe RunnerRoullette::Table do
   end
 
   its "1st vertical row should have a count of 8 items" do
-    table.vertical_rows[0].count.should == 8
+    table.columns[0].count.should == 8
   end
 
   its "2nd vertical row should have a count of 7 items" do
-    table.vertical_rows[1].count.should == 7
+    table.columns[1].count.should == 7
   end
 
   its "3rd vertical row should have a count of 7 items" do
-    table.vertical_rows[2].count.should == 7
+    table.columns[2].count.should == 7
   end
 
   its "1st Column should have a decimal price of 1.92" do
